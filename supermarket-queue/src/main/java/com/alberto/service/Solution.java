4647cbdaaf4185a8274bd529d15a8e0facfaf866
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2017-05-12 10:55:56 +0200

    Solucion mas eficaz

--- supermarket-queue/src/main/java/com/alberto/service/Solution.java
+++ supermarket-queue/src/main/java/com/alberto/service/Solution.java
@@ -1,34 +1,50 @@
 package com.alberto.service;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Solution {
 
 	private static int tiempoTotal = 0;
+	private static List<Integer> listaColas = new ArrayList<>();
+	private static boolean colaAsignada = false;
 
 	public static Object solveSuperMarketQueue(int[] customers, int numberQueues) {
 		tiempoTotal = 0;
-		List<Integer> listaColas = new ArrayList<>();
+		listaColas = new ArrayList<>();
+		colaAsignada = false;
 		for (int i = 0; i < numberQueues; i++) {
 			listaColas.add(0);
 		}
-		for (int customerTime : customers) {
+		Arrays.stream(customers).forEach(customerTime -> {
 			int minCola = Collections.min(listaColas);
 			tiempoTotal += minCola;
-			boolean colaAsignada = false;
-			for (int i = 0; i < listaColas.size(); i++) {
-				if (listaColas.get(i) == minCola && !colaAsignada) {
-					listaColas.set(i, customerTime);
+			listaColas = listaColas.stream().map(cola -> {
+				if (cola == minCola && !colaAsignada) {
+					cola = customerTime;
 					colaAsignada = true;
 				} else {
-					listaColas.set(i, listaColas.get(i) - minCola);
+					cola = cola - minCola;
 				}
-			}
-		}
+				return cola;
+			}).collect(Collectors.toList());
+			colaAsignada = false;
+		});
 		tiempoTotal += Collections.max(listaColas);
+		Arrays.sort(customers);
 		return tiempoTotal;
 	}
+
+//	public static int solveSuperMarketQueue(int[] customers, int n) {
+//		int[] result = new int[n];
+//		for (int i = 0; i < customers.length; i++) {
+//			result[0] += customers[i];
+//			Arrays.sort(result);
+//		}
+//		return result[n - 1];
+//	}
 
 }
