4234d5491ed44034411194b08afdfb197ab9bb56
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2017-06-09 12:37:56 +0200

    Ultimas modificaciones
    Aun no funciona

--- hamming-numbers/index.js
+++ hamming-numbers/index.js
@@ -1,7 +1,30 @@
 'use strict';
 
+const isHammingNumber = (num) => {
+  while (num % 5 === 0) {
+    num /= 5;
+  }
+  while (num % 3 === 0) {
+    num /= 3;
+  }
+  while (num % 2 === 0) {
+    num /= 2;
+  }
+  return num === 1;
+}
+
 module.exports = {
   hamming(number) {
-    return 1;
+    var succession = [ 1 ];
+    var length = succession.length;
+    var candidate = 2;
+    while (length < number) {
+      if (isHammingNumber(candidate)) {
+        succession[length] = candidate;
+        length++;
+      }
+      candidate++;
+    }
+    return succession[succession.length - 1];
   }
 };
